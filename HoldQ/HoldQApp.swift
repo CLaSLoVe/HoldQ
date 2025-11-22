a6c8861c06f7d3cb9216de770a10b882907cbebd
dian <jindian_0805@foxmail.com>
2025-11-22 13:23:54 +0800

    Initial Commit

---
+++ HoldQ/HoldQApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldQApp.swift
+//  HoldQ
+//
+//  Created by Dian Jin on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HoldQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
